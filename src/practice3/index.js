50b3acfa05107f0d76be28c20678cacc2c898bed
Sunny Yang <sunnyadn@foxmail.com>
2020-10-27 14:35:56 +0800

    Finish homework

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,17 @@
-export const getPoetry
+const URL = 'https://v1.jinrishici.com/all.json';
+
+const getPoetryByPromise = function() {
+    return fetch(URL)
+        .then(response => response.json())
+        .then(poetry => [poetry.origin, poetry.author, poetry.content])
+        .catch(console.log);
+}
+
+const getPoetryByAsync =  async function() {
+    const response = await fetch(URL);
+    const poetry = await response.json();
+    const { origin, author, content } = poetry;
+    return [origin, author, content];
+}
+
+export const getPoetry = getPoetryByAsync;
